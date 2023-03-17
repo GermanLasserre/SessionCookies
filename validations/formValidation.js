955b0f983f06a2179032a3bd87d1367d51e24299
germanlasserre <germanlasserre@gmail.com>
2023-03-17 05:35:35 -0300

    Creo repositoriot

---
+++ validations/formValidation.js
@@ -0,0 +1,19 @@
+const {check} = require("express-validator");
+
+module.exports = [
+    check("name")
+    .notEmpty()
+    .withMessage("Debes completar este campo"),
+
+    check("color")
+    .notEmpty()
+    .withMessage("Debes elegir un color"),
+    
+    check("email")
+    .isEmail()
+    .withMessage("Debes completar con un email valido"),
+
+    check("age")
+    .isNumeric()
+    .withMessage()
+]
